eea4fa6a099f94ed59383a4c360b1a01e63768e3
Luis Navarrete <luis.e.415@gmail.com>
2020-03-31 15:35:06 -0600

    Termino del curso NODE_JS
    Se apendio a usar NPM para intalar paquetes  modulos externos. y ver las depedncias.

--- INTORDUCCION/math.js
+++ INTORDUCCION/math.js
@@ -1,3 +1,4 @@
+//Creamos un objeto y lo exportamos con module.exports
 const Math = {};
 
 function sumar(x1,x2) {
@@ -17,11 +18,27 @@ function dividir(x1,x2) {
     }
 }
 
+//Vamos a exportar alguna funciòn llamada ADD
+/*
+exports.add = sumar;
+*/
 
+
+//Del objeto, tomamos la propiedad sumar y decimos que use la función "sumar"
+//Asi para cada una de las propiedades. 
 
 Math.sumar = sumar;
 Math.restar = restar;
 Math.multiplicar = multiplicar;
 Math.dividir = dividir;
 
+/*
+function hello(name) {
+    console.log("Hola, "+name);
+}
+*/
+
+//Exportamos el modulo como tal.
 module.exports = Math;
+//module.exports = hello;
+
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const math = require("./math.js");
+/* const http = require("http");
+const colors = require("colors");
 
-console.log(math);
 
-/*
-console.log(math.dividir(1,2));
-console.log(math.sumar(1,2));
-console.log(math.restar(1,2));
-console.log(math.multiplicar(1,2));
-*/
+const handleServer = function (req, res) {
+    res.writeHead(200, {"content-type": 'text/html'});
+    res.write("<h1>Hola Mundo </h1>");
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function () {
+    console.log('Server on port 3000'.yellow);
+});
+
+ */
+
+const express = require("express");
+const colors = require("colors");
+const server = express();
+
+server.get("/", function(req,res){
+    res.send("<h1>Hola Mundo con Express y Node</h1>");
+    res.end();
+});
+
+server.listen(3000, function(){
+    console.log("Server on port 3000".red);
+});
+
+
+
