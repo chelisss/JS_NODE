7b9797ae25fe775791de81361f7582496d6adca1
Luis Navarrete <luis.e.415@gmail.com>
2020-04-03 16:44:38 -0600

    Curso termiando de NODE JS REST API
    Sea gregaron y consultamos un JSON predefinido

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ app.set("json spaces",2);
 
 //Middleware
 app.use(morgan("dev"));
-
 app.use(express.json());//Nos permite recibir formatos JSON
 app.use(express.urlencoded({extended: false})); //Nos permite entender datos que vienen de formularios
 
@@ -17,10 +16,10 @@ app.use(express.urlencoded({extended: false})); //Nos permite entender datos que
 //Routes
 app.use(require('./routes/index'));
 app.use('/api/movies',require('./routes/movies'));
-
+app.use('/api/users', require('./routes/users'));
 
 
 //Starting the server
 app.listen(app.get("port"), () => {
-    console.log(`Server on port ${3000}`);
+    console.log(`Server on port ${app.get('port')}`);
 });
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,16 +1,57 @@
 const {Router} = require("express");
 const router = Router();
+const _ = require("underscore");
 
 const peliculas = require("../sample.json");
 //console.log(peliculas);
 
 router.get("/", (req,res) =>{
-    res.send(peliculas);
+    res.json(peliculas);
 });
 
 router.post('/', (req,res) => {
-    console.log(req.body);
-    res.send("Recibido");
+    const {title, director, year, rating } = req.body;
+    if(title && director && year && rating){
+        const id = peliculas.length +1;
+        const newMovie = {...req.body, id}
+        //console.log(newMovie);
+        peliculas.push(newMovie);
+        res.json(peliculas);
+    }else{
+        res.status(500).json({error:"Hubo un error"}); 
+    }
+});
+
+
+router.delete("/:id", (req,res) =>{
+    const {id} = req.params;
+
+    _.each(peliculas, (pelicula, i) =>{
+        if(pelicula.id == id){
+            peliculas.splice(i,1);
+        }
+    });
+    res.send(peliculas);
+});
+
+router.put("/:id", (req, res)=>{
+    const {id} = req.params;
+
+    const { title, director, year, rating} = req.body;
+    if(title && director&& year && rating){
+        _.each(peliculas, (movie, i)=>{
+            if(movie.id == id){
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(peliculas);
+    }else{
+        res.status(500).json({error: "Hubo un error."});
+    }
+ 
 });
 
 module.exports = router;
